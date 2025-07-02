986ee5ada33dd881694584c9542f215cd049d47e
Kunaal Solanki <solankikunaal6@gmail,com>
2025-07-02 20:28:59 +0530

    Added a typewrite effect

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Hero.css';
 import profile_img from '../../assets/KunaalAiImage-modified.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Typewriter from 'typewriter-effect/dist/core';
 
 const Hero = () => {
+    useEffect(() => {
+        const typewriter = new Typewriter('#typewriter', {
+            strings: ["Hello, I'm Kunaal Solanki,"],
+            autoStart: true,
+            loop: true,
+            delay: 75,
+        });
+    }, []);
+
     return (
         <div id='home' className="hero">
             <img src={profile_img} alt="Profile" />
-            <h1><span>Hello, I'm Kunaal Solanki,</span> crafting code from Mumbai.</h1>
-            <p>I'm a full stack developer driven by curiosity and creativity—bringing ideas to life through elegant and efficient web solutions.</p>
+            <h1><span id="typewriter"></span> crafting code from Mumbai.</h1>
+            <p>
+                I'm a full stack developer driven by curiosity and creativity—bringing ideas to life through elegant and efficient web solutions.
+            </p>
             <div className="hero-actions">
                 <div className="hero-connect">
                     <AnchorLink className='anchor-link' offset={50} href='#contact'>
@@ -24,7 +36,7 @@ const Hero = () => {
                 </a>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Hero;
